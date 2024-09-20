b169e1f2304273f709adf01781fbd58eda8db31a
Dimas Aditya Herlambang <dimas070906@gmail.com>
2024-09-20 14:36:49 +0700

    category & brand recource completed

---
+++ app/Filament/Resources/BrandResource.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BrandResource\Pages;
+use App\Filament\Resources\BrandResource\RelationManagers;
+use App\Models\Brand;
+use Filament\Forms;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Form;
+use Filament\Forms\Set;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
+
+
+class BrandResource extends Resource
+{
+    protected static ?string $model = Brand::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+    ->schema([
+        Section::make([
+            Grid::make()
+                ->schema([
+                    Forms\Components\TextInput::make('name')
+                        ->required()
+                        ->maxLength(255)
+                        ->live()
+                        ->afterStateUpdated(fn(string $operation, $state, Set $set) => $operation
+                            === 'create' ? $set('slug', Str::slug($state)) : null),
+
+                    Forms\Components\TextInput::make('slug')
+                        ->required()
+                        ->maxLength(255)
+                        ->readOnly()
+                        ->dehydrated()
+                        ->unique(Brand::class, 'slug', ignoreRecord: true),
+                ])
+                ->columns(2), // Mengatur kolom untuk name dan slug
+
+            Forms\Components\FileUpload::make('image')
+                ->image()
+                ->directory('brands'),
+
+            Forms\Components\Toggle::make('is_active')
+                ->required()
+                ->default(true),
+        ])
+    ]);
+
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+        ->columns([
+            Tables\Columns\TextColumn::make('nomor')
+                ->label('No')
+                ->getStateUsing(fn ($rowLoop) => $rowLoop->index + 1),
+            Tables\Columns\TextColumn::make('name')
+                ->searchable(),
+            Tables\Columns\ImageColumn::make('image')
+            ->size(50,50),
+            Tables\Columns\TextColumn::make('slug')
+                ->searchable()
+                ->toggleable(isToggledHiddenByDefault: true),
+            Tables\Columns\IconColumn::make('is_active')
+                ->boolean(),
+            Tables\Columns\TextColumn::make('created_at')
+                ->dateTime()
+                ->sortable()
+                ->toggleable(isToggledHiddenByDefault: true),
+            Tables\Columns\TextColumn::make('updated_at')
+                ->dateTime()
+                ->sortable()
+                ->toggleable(isToggledHiddenByDefault: true),
+        ])
+        ->filters([
+            //
+        ])
+        ->actions([
+            Tables\Actions\ActionGroup::make([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(), 
+            ])
+        ])
+        ->bulkActions([
+            Tables\Actions\BulkActionGroup::make([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]),
+        ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBrands::route('/'),
+            'create' => Pages\CreateBrand::route('/create'),
+            'edit' => Pages\EditBrand::route('/{record}/edit'),
+        ];
+    }
+}
