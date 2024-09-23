edf2d9622f631662760c3bc8d741986ff8e37cbd
Dimas Aditya Herlambang <dimas070906@gmail.com>
2024-09-23 10:10:50 +0700

    Create Login Panel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Livewire\Auth\ForgotPasswordPage;
+use App\Livewire\Auth\LoginPage;
+use App\Livewire\Auth\RegisterPage;
+use App\Livewire\Auth\ResetPasswordPage;
+use App\Livewire\CancelPage;
 use App\Livewire\CartPage;
 use App\Livewire\CategoriesPage;
 use App\Livewire\CheckoutPage;
@@ -8,6 +13,7 @@ use App\Livewire\MyOrderDetailPage;
 use App\Livewire\OrdersPage;
 use App\Livewire\ProductDetailPage;
 use App\Livewire\ProductsPage;
+use App\Livewire\SuccessPage;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,7 +32,16 @@ Route::get("/categories", CategoriesPage::class);
 Route::get("/products", ProductsPage::class);
 Route::get("/products/{product}", ProductDetailPage ::class);
 Route::get("/cart", CartPage::class);
-Route::get("/checkout", CheckoutPage::class);
 
+Route::get("/checkout", CheckoutPage::class);
 Route::get("/my-orders", OrdersPage::class);
 Route::get("/my-orders/{order}", MyOrderDetailPage::class);
+
+Route::get("/login", LoginPage::class);
+Route::get("/register", RegisterPage::class);
+Route::get("/forgot", ForgotPasswordPage::class);
+Route::get("/reset", ResetPasswordPage::class);
+
+
+Route::get("/success", SuccessPage::class);
+Route::get("/cancel", CancelPage::class);
